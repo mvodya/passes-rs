3961da0a1215ea4b3af01ee50c043bc95c474da3
Mark Vodyanitskiy <mvodya@icloud.com>
2024-02-10 21:03:10 +1000

    Added error handler for Color

--- src/pass/visual_appearance.rs
+++ src/pass/visual_appearance.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize};
 
 /// Visual appearance of a pass
 #[derive(Serialize, Deserialize, Debug)]
@@ -49,17 +49,22 @@ pub struct Color {
 
 impl Color {
     /// Creates a new `Color`.
-    fn new(r: u8, g: u8, b: u8) -> Self {
-        Self { r, g, b }
+    fn new(r: u8, g: u8, b: u8) -> Option<Self> {
+        Some(Self { r, g, b })
     }
 
     /// Creates a white `Color`.
-    fn white() -> Self {
-        Self {
+    fn black() -> Option<Self> {
+        Some(Self { r: 0, g: 0, b: 0 })
+    }
+
+    /// Creates a white `Color`.
+    fn white() -> Option<Self> {
+        Some(Self {
             r: 255,
             g: 255,
             b: 255,
-        }
+        })
     }
 }
 
@@ -82,21 +87,23 @@ impl<'de> Deserialize<'de> for Color {
     {
         let str = String::deserialize(deserializer)?;
 
+        // Parse triple `rgb(red, blue, green)`
         let re = Regex::new(r"rgb\((?P<r>\d+),\s*(?P<g>\d+),\s*(?P<b>\d+)\)").unwrap();
-        let captures = re.captures(&str).unwrap();
+        let captures = re.captures(&str);
 
-        let r = captures["r"].parse::<u8>().unwrap();
-        let g = captures["g"].parse::<u8>().unwrap();
-        let b = captures["b"].parse::<u8>().unwrap();
-
-        Ok(Self { r, g, b })
-    }
-}
-
-impl Default for Color {
-    /// Creates a black `Color`.
-    fn default() -> Self {
-        Self { r: 0, g: 0, b: 0 }
+        if let Some(captures) = captures {
+            if let (Ok(r), Ok(g), Ok(b)) = (
+                captures["r"].parse::<u8>(),
+                captures["g"].parse::<u8>(),
+                captures["b"].parse::<u8>(),
+            ) {
+                Ok(Self { r, g, b })
+            } else {
+                Err(de::Error::custom("Invalid color arguments"))
+            }
+        } else {
+            Err(de::Error::custom("Invalid color format"))
+        }
     }
 }
 
@@ -107,9 +114,9 @@ mod tests {
     #[test]
     fn make_appearance() {
         let appearance = VisualAppearance {
-            label_color: Some(Color::new(255, 100, 100)),
-            foreground_color: Some(Color::new(255, 100, 100)),
-            background_color: Some(Color::new(255, 100, 100)),
+            label_color: Color::new(255, 100, 100),
+            foreground_color: Color::new(255, 100, 100),
+            background_color: Color::new(255, 100, 100),
             logo_text: Some(String::from("Test Logo")),
         };
 
@@ -128,10 +135,8 @@ mod tests {
     }
 
     #[test]
-    fn make_new_color() {
-        let color = Color::new(100, 200, 240);
-
-        println!("{:?}", color);
+    fn make_custom_color() {
+        let color = Color::new(100, 200, 240).unwrap();
 
         assert_eq!(100, color.r);
         assert_eq!(200, color.g);
@@ -140,7 +145,7 @@ mod tests {
 
     #[test]
     fn make_black_color() {
-        let color = Color::default();
+        let color = Color::black().unwrap();
 
         println!("{:?}", color);
 
@@ -151,12 +156,60 @@ mod tests {
 
     #[test]
     fn make_white_color() {
-        let color = Color::white();
+        let color = Color::white().unwrap();
 
         println!("{:?}", color);
 
         assert_eq!(255, color.r);
         assert_eq!(255, color.g);
         assert_eq!(255, color.b);
+    }
+
+    #[test]
+    fn color_serialization() {
+        let color = Color::new(12, 34, 56).unwrap();
+
+        let json = serde_json::to_string_pretty(&color).unwrap();
+
+        let json_expected = r#""rgb(12, 34, 56)""#;
+
+        assert_eq!(json_expected, json);
+    }
+
+    #[test]
+    fn color_deserialization() {
+        let json = r#""rgb(12, 34, 56)""#;
+
+        let color: Color = serde_json::from_str(json).unwrap();
+
+        let expected_color = Color::new(12, 34, 56).unwrap();
+
+        assert_eq!(expected_color.r, color.r);
+        assert_eq!(expected_color.g, color.g);
+        assert_eq!(expected_color.b, color.b);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid color format")]
+    fn color_deserialization_error() {
+        let json = r#""rgb(12, 34, abc)""#;
+
+        let _: Color = serde_json::from_str(json).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid color arguments")]
+    fn color_incorrect_args_0() {
+        let json = r#""rgb(12, 500, 0)""#;
+
+        let _: Color = serde_json::from_str(json).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid color arguments")]
+    fn color_incorrect_args_1() {
+        let json = r#""rgb(0, 256, 0)""#;
+
+        let _: Color = serde_json::from_str(json).unwrap();
     }
 }
