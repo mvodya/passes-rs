80ea50371608285e169c71eee80d362da22a7269
Mark Vodyanitskiy <mvodya@icloud.com>
2024-02-21 13:32:10 +1000

    Support multiple datetime formats

--- src/pass/date_format.rs
+++ src/pass/date_format.rs
@@ -1,14 +1,12 @@
 use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{self, Deserialize, Deserializer, Serializer};
 
-const FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%:z";
-
 /// Serialization to custom date format
 pub fn serialize<S>(date: &Option<DateTime<Utc>>, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    let s = format!("{}", date.unwrap().format(FORMAT));
+    let s = format!("{}", date.unwrap().to_rfc3339());
     serializer.serialize_str(&s)
 }
 
@@ -18,8 +16,18 @@ where
     D: Deserializer<'de>,
 {
     let s = String::deserialize(deserializer)?;
-    let dt = NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)?;
-    Ok(Some(DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc)))
+    if let Ok(dt) = DateTime::parse_from_rfc3339(&s) {
+        // RFC3339
+        Ok(Some(dt.with_timezone(&Utc)))
+    } else if let Ok(dt) = DateTime::parse_from_rfc2822(&s) {
+        // RFC2822
+        Ok(Some(dt.with_timezone(&Utc)))
+    } else if let Ok(dt) = NaiveDateTime::parse_from_str(&s, "%Y-%m-%dT%H:%M:%S") {
+        // Custom naive format
+        Ok(Some(DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc)))
+    } else {
+        Err(serde::de::Error::custom("Invalid DateTime format"))
+    }
 }
 
 #[cfg(test)]
@@ -50,10 +58,30 @@ mod tests {
     }
 
     #[test]
-    fn full_deserialize_check() {
+    fn rfc3339_deserialize_check() {
         let json = r#"{
   "date": "2024-02-07T10:15:00+00:00"
 }"#;
+        let date_struct: DateTest = serde_json::from_str(json).unwrap();
+        let date_expected = Utc.with_ymd_and_hms(2024, 02, 07, 10, 15, 0).unwrap();
+        assert_eq!(date_expected, date_struct.date.unwrap());
+    }
+
+    #[test]
+    fn rfc2822_deserialize_check() {
+        let json = r#"{
+  "date": "Wed, 07 Feb 2024 10:15:00 GMT"
+}"#;
+        let date_struct: DateTest = serde_json::from_str(json).unwrap();
+        let date_expected = Utc.with_ymd_and_hms(2024, 02, 07, 10, 15, 0).unwrap();
+        assert_eq!(date_expected, date_struct.date.unwrap());
+    }
+
+    #[test]
+    fn no_tz_deserialize_check() {
+        let json = r#"{
+      "date": "2024-02-07T10:15:00"
+    }"#;
         let date_struct: DateTest = serde_json::from_str(json).unwrap();
         let date_expected = Utc.with_ymd_and_hms(2024, 02, 07, 10, 15, 0).unwrap();
         assert_eq!(date_expected, date_struct.date.unwrap());
