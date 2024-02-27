2c84220ccce18c6d207803da29899f1fde3fca87
Mark Vodyanitskiy <mvodya@icloud.com>
2024-02-27 17:46:57 +1000

    Created prototype example of pass generator with signing

--- examples/pass-generator/src/main.rs
+++ examples/pass-generator/src/main.rs
@@ -1,4 +1,5 @@
 use passes::package::resource;
+use passes::package::sign::SignConfig;
 use passes::package::Package;
 use passes::pass::{PassBuilder, PassConfig};
 
@@ -26,12 +27,37 @@ fn main() {
 
     let image_path = Path::new("template_app_icon.png");
     let file = match File::open(&image_path) {
-        Err(why) => panic!("couldn't create {}: {}", image_path.display(), why),
+        Err(why) => panic!("couldn't open {}: {}", image_path.display(), why),
         Ok(file) => file,
     };
     package
         .add_resource(resource::Type::Logo(resource::Version::Standard), file)
         .unwrap();
+
+    // Add certificates
+    let sign_cert_path = Path::new("certs/signerCert.pem");
+    let mut file_sign_cert = match File::open(&sign_cert_path) {
+        Err(why) => panic!("couldn't open {}: {}", sign_cert_path.display(), why),
+        Ok(file) => file,
+    };
+    let mut sign_cert_data = Vec::new();
+    std::io::Read::read_to_end(&mut file_sign_cert, &mut sign_cert_data).unwrap();
+
+    let sign_cert_key_path = Path::new("certs/signerKey.key");
+    let mut file_sign_key_cert = match File::open(&sign_cert_key_path) {
+        Err(why) => panic!("couldn't open {}: {}", sign_cert_key_path.display(), why),
+        Ok(file) => file,
+    };
+    let mut sign_cert_key_data = Vec::new();
+    std::io::Read::read_to_end(&mut file_sign_key_cert, &mut sign_cert_key_data).unwrap();
+
+    let sign_config = SignConfig::new(
+        passes::package::sign::WWDR::G4,
+        &sign_cert_data,
+        &sign_cert_key_data,
+    )
+    .unwrap();
+    package.add_certificates(sign_config);
 
     // Save package as .pkpass
     let path = Path::new("test_pass.pkpass");
