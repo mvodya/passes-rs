f553ec979983c142c7568718a59807a4202b10af
Mark Vodyanitskiy <mvodya@icloud.com>
2024-02-28 12:00:09 +1000

    Fix example generator problems

--- examples/pass-generator/src/main.rs
+++ examples/pass-generator/src/main.rs
@@ -9,12 +9,13 @@ use std::path::Path;
 fn main() {
     // Creating pass
     let pass = PassBuilder::new(PassConfig {
-        organization_name: "Apple inc.".into(),
-        description: "Example pass".into(),
+        organization_name: "Test organization".into(),
+        description: "Super gentlememe pass".into(),
         pass_type_identifier: "com.example.pass".into(),
         team_identifier: "AA00AA0A0A".into(),
         serial_number: "ABCDEFG1234567890".into(),
     })
+    .grouping_identifier(String::from("com.example.pass.app"))
     .logo_text("Test pass".into())
     .build();
 
@@ -25,13 +26,14 @@ fn main() {
     // Creating package
     let mut package = Package::new(pass);
 
+    // Icon for pass required (!)
     let image_path = Path::new("template_app_icon.png");
     let file = match File::open(&image_path) {
         Err(why) => panic!("couldn't open {}: {}", image_path.display(), why),
         Ok(file) => file,
     };
     package
-        .add_resource(resource::Type::Logo(resource::Version::Standard), file)
+        .add_resource(resource::Type::Icon(resource::Version::Standard), file)
         .unwrap();
 
     // Add certificates
@@ -43,6 +45,7 @@ fn main() {
     let mut sign_cert_data = Vec::new();
     std::io::Read::read_to_end(&mut file_sign_cert, &mut sign_cert_data).unwrap();
 
+    // Setup certificates
     let sign_cert_key_path = Path::new("certs/signerKey.key");
     let mut file_sign_key_cert = match File::open(&sign_cert_key_path) {
         Err(why) => panic!("couldn't open {}: {}", sign_cert_key_path.display(), why),
